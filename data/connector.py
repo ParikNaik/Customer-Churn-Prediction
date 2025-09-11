833d425130719dffe11a3dcebb658c5f4317edae
ParikNaik <132858911+ParikNaik@users.noreply.github.com>
2025-09-11 12:09:33 -0400

    test connector

--- data/connector.py
+++ data/connector.py
@@ -1,2 +1,22 @@
-import mysql_connector
+import mysql.connector
 
+Customerdb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="Deadlygamer905!",
+      database="churn_database")
+
+cursor = Customerdb.cursor()
+
+cursor.execute("SELECT * FROM customerdata LIMIT 1;")
+
+    # Fetch all rows
+myresult = cursor.fetchone()
+
+    # Iterate and print the data
+for x in myresult:
+      print(x)
+
+cursor.close()
+
+Customerdb.close()
